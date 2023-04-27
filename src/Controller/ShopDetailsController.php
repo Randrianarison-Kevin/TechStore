58fbd6788d791489927e1d8b88b5ad2cac14d38a
Randrianarison-Kevin <randrianarison.noavintsoa.kevin@gmail.com>
2023-04-28 00:14:56 +0300

    Shop_Details

--- src/Controller/ShopDetailsController.php
+++ src/Controller/ShopDetailsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class ShopDetailsController extends AbstractController
 {
     #[Route('/shopdetails', name: 'shop_details')]
-    public function index(): Response
+    public function index(ProduitRepository $produitRepository): Response
     {
+        $produits= $produitRepository->findAll();
+        $produit = $produits[array_rand($produits)];
         return $this->render('pages/shop-details.html.twig', [
+            'Produit'=>$produit
         ]);
     }
 }
