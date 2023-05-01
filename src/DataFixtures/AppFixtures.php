7c5fd51e13a1a76702673fb278cb4b261137dbbc
Randrianarison-Kevin <randrianarison.noavintsoa.kevin@gmail.com>
2023-05-01 23:14:39 +0300

    Login

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,11 +4,24 @@ namespace App\DataFixtures;
 
 use App\Entity\Categories;
 use App\Entity\Produit;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use Faker\Generator;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private Generator $faker;
+
+
+    public function __construct()
+    {
+        $this ->faker = Factory::create('fr_FR');
+    }
+
     public function load(ObjectManager $manager): void
     {
 
@@ -32,6 +45,14 @@ class AppFixtures extends Fixture
            
         }
        
+        for ($i=0; $i < 10 ; $i++) { 
+            $user = new User;
+            $user   -> setUsername($this->faker->name())
+                    ->setEmail($this ->faker ->email())
+                    ->setRoles(['ROLE_USER'])
+                    ->setPlainPassword($this->faker -> password());
+            $manager ->persist($user);
+        }
         
         
 
